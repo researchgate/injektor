ac86939c81ae431ecdccfdbb49852dab54a716c3
Bastian Hofmann <bashofmann@googlemail.com>
2012-04-14 12:37:00 +0100

    added license information

--- src/rg/injection/generators/InjectionParameter.php
+++ src/rg/injection/generators/InjectionParameter.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of rg\injection.
+ *
+ * (c) ResearchGate GmbH <bastian.hofmann@researchgate.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace rg\injection\generators;
 
 use rg\injection\InjectionException;
--- test/rg/injection/aspects/MethodTest.php
+++ test/rg/injection/aspects/MethodTest.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of rg\injection.
+ *
+ * (c) ResearchGate GmbH <bastian.hofmann@researchgate.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace rg\injection\aspects;
 
 class MethodTest extends \PHPUnit_Framework_TestCase {
