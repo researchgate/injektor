310c863c66bb0e1ae874a1ad919242f595f966e5
Bastian Hofmann <bashofmann@googlemail.com>
2012-04-14 12:29:16 +0100

    added initial composer file for dependencies

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -7,12 +7,4 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-require_once(__DIR__ . '/../vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php');
-
-$loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-$loader->registerNamespaces(array(
-    'Symfony' => __DIR__ . '/../vendor',
-    'Zend' => __DIR__ . '/../vendor',
-    'rg' => __DIR__,
-));
-$loader->register();
+require __DIR__ . '/../vendor/.composer/autoload.php';
