a59868bdbb83ec8bb2ead63037a44b709c01663c
Bastian Hofmann <bashofmann@googlemail.com>
2012-05-01 13:48:54 +0200

    compatibility changes for newest composer version

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -7,4 +7,4 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-require __DIR__ . '/../vendor/.composer/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
